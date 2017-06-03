cc98017e4ceba609fef32f647550ef27805873eb
Ilya Razenshteyn <ilyaraz@mit.edu>
2017-06-03 17:49:51 -0400

    less of code duplication

--- gen.py
+++ gen.py
@@ -1,83 +1,83 @@
-def float_avx_0(from_register, to_register, aux_registers, ident=''):
+def float_avx_0(register, aux_registers, ident=''):
     """
     from_register can be the same as to_register, but both must be disjoint from aux_registers
     """
-    if len(aux_registers) != 4:
+    if len(aux_registers) < 4:
         raise Exception('need four auxiliary registers')
-    res  = ident + '"vpermilps $160, %s, %s\\n"\n' % (from_register, aux_registers[0])
-    res += ident + '"vpermilps $245, %s, %s\\n"\n' % (from_register, aux_registers[1])
-    res += ident + '"vxorps %s, %s, %s\\n"\n' % (aux_registers[2], aux_registers[2], aux_registers[2])
-    res += ident + '"vsubps %s, %s, %s\\n"\n' % (aux_registers[1], aux_registers[2], aux_registers[3])
-    res += ident + '"vaddsubps %s, %s, %s\\n"\n' % (aux_registers[3], aux_registers[0], to_register)
+    res  = ident + '"vpermilps $160, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[0])
+    res += ident + '"vpermilps $245, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[1])
+    res += ident + '"vxorps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[2], aux_registers[2], aux_registers[2])
+    res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[1], aux_registers[2], aux_registers[3])
+    res += ident + '"vaddsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[3], aux_registers[0], register)
     return res
 
-def float_avx_1(from_register, to_register, aux_registers, ident=''):
+def float_avx_1(register, aux_registers, ident=''):
     """
     from_register can be the same as to_register, but both must be disjoint from aux_registers
     """
-    if len(aux_registers) != 5:
+    if len(aux_registers) < 5:
         raise Exception('need five auxiliary registers')
-    res  = ident + '"vpermilps $68, %s, %s\\n"\n' % (from_register, aux_registers[0])
-    res += ident + '"vpermilps $238, %s, %s\\n"\n' % (from_register, aux_registers[1])
-    res += ident + '"vxorps %s, %s, %s\\n"\n' % (aux_registers[2], aux_registers[2], aux_registers[2])
-    res += ident + '"vsubps %s, %s, %s\\n"\n' % (aux_registers[1], aux_registers[2], aux_registers[3])
-    res += ident + '"vblendps $204, %s, %s, %s\\n"\n' % (aux_registers[3], aux_registers[1], aux_registers[4])
-    res += ident + '"vaddps %s, %s, %s\\n"\n' % (aux_registers[0], aux_registers[4], to_register)
+    res  = ident + '"vpermilps $68, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[0])
+    res += ident + '"vpermilps $238, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[1])
+    res += ident + '"vxorps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[2], aux_registers[2], aux_registers[2])
+    res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[1], aux_registers[2], aux_registers[3])
+    res += ident + '"vblendps $204, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[3], aux_registers[1], aux_registers[4])
+    res += ident + '"vaddps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[0], aux_registers[4], register)
     return res
 
-def float_avx_2(from_register, to_register, aux_registers, ident=''):
+def float_avx_2(register, aux_registers, ident=''):
     """
     from_register can be the same as to_register, but both must be disjoint from aux_registers
     """
-    if len(aux_registers) != 4:
+    if len(aux_registers) < 4:
         raise Exception('need four auxiliary registers')
-    res  = ident + '"vxorps %s, %s, %s\\n"\n' % (aux_registers[0], aux_registers[0], aux_registers[0])
-    res += ident + '"vsubps %s, %s, %s\\n"\n' % (from_register, aux_registers[0], aux_registers[1])
-    res += ident + '"vperm2f128 $0, %s, %s, %s\\n"\n' % (from_register, from_register, aux_registers[2])
-    res += ident + '"vperm2f128 $49, %s, %s, %s\\n"\n' % (aux_registers[1], from_register, aux_registers[3])
-    res += ident + '"vaddps %s, %s, %s\\n"\n' % (aux_registers[2], aux_registers[3], to_register)
+    res  = ident + '"vxorps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[0], aux_registers[0], aux_registers[0])
+    res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[0], aux_registers[1])
+    res += ident + '"vperm2f128 $0, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (register, register, aux_registers[2])
+    res += ident + '"vperm2f128 $49, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[1], register, aux_registers[3])
+    res += ident + '"vaddps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[2], aux_registers[3], register)
     return res
 
 def float_avx_3_etc(from_register_1, from_register_2, to_register_1, to_register_2, ident=''):
     """
     all four registers must be distinct
     """
-    res  = ident + '"vaddps %s, %s, %s\\n"\n' % (from_register_2, from_register_1, to_register_1)
-    res += ident + '"vsubps %s, %s, %s\\n"\n' % (from_register_2, from_register_1, to_register_2)
+    res  = ident + '"vaddps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (from_register_2, from_register_1, to_register_1)
+    res += ident + '"vsubps %%%%%s, %%%%%s, %%%%%s\\n"\n' % (from_register_2, from_register_1, to_register_2)
     return res
 
-def double_avx_0(from_register, to_register, aux_registers, ident=''):
+def double_avx_0(register, aux_registers, ident=''):
     """
     from_register can be the same as to_register, but both must be disjoint from aux_registers
     """
-    if len(aux_registers) != 4:
+    if len(aux_registers) < 4:
         raise Exception('need four auxiliary registers')
-    res  = ident + '"vpermilpd $0, %s, %s\\n"\n' % (from_register, aux_registers[0])
-    res += ident + '"vpermilpd $15, %s, %s\\n"\n' % (from_register, aux_registers[1])
-    res += ident + '"vxorpd %s, %s, %s\\n"\n' % (aux_registers[2], aux_registers[2], aux_registers[2])
-    res += ident + '"vsubpd %s, %s, %s\\n"\n' % (aux_registers[1], aux_registers[2], aux_registers[3])
-    res += ident + '"vaddsubpd %s, %s, %s\\n"\n' % (aux_registers[3], aux_registers[0], to_register)
+    res  = ident + '"vpermilpd $0, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[0])
+    res += ident + '"vpermilpd $15, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[1])
+    res += ident + '"vxorpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[2], aux_registers[2], aux_registers[2])
+    res += ident + '"vsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[1], aux_registers[2], aux_registers[3])
+    res += ident + '"vaddsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[3], aux_registers[0], register)
     return res
 
-def double_avx_1(from_register, to_register, aux_registers, ident=''):
+def double_avx_1(register, aux_registers, ident=''):
     """
     from_register can be the same as to_register, but both must be disjoint from aux_registers
     """
-    if len(aux_registers) != 4:
+    if len(aux_registers) < 4:
         raise Exception('need four auxiliary registers')
-    res  = ident + '"vperm2f128 $0, %s, %s, %s\\n"\n' % (from_register, from_register, aux_registers[0])
-    res += ident + '"vxorpd %s, %s, %s\\n"\n' % (aux_registers[1], aux_registers[1], aux_registers[1])
-    res += ident + '"vsubpd %s, %s, %s\\n"\n' % (from_register, aux_registers[1], aux_registers[2])
-    res += ident + '"vperm2f128 $49, %s, %s, %s\\n"\n' % (aux_registers[2], from_register, aux_registers[3])
-    res += ident + '"vaddpd %s, %s, %s\\n"\n' % (aux_registers[3], aux_registers[0], to_register)
+    res  = ident + '"vperm2f128 $0, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (register, register, aux_registers[0])
+    res += ident + '"vxorpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[1], aux_registers[1], aux_registers[1])
+    res += ident + '"vsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (register, aux_registers[1], aux_registers[2])
+    res += ident + '"vperm2f128 $49, %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[2], register, aux_registers[3])
+    res += ident + '"vaddpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (aux_registers[3], aux_registers[0], register)
     return res
 
 def double_avx_2_etc(from_register_1, from_register_2, to_register_1, to_register_2, ident=''):
     """
     all four registers must be distinct
     """
-    res  = ident + '"vaddpd %s, %s, %s\\n"\n' % (from_register_2, from_register_1, to_register_1)
-    res += ident + '"vsubpd %s, %s, %s\\n"\n' % (from_register_2, from_register_1, to_register_2)
+    res  = ident + '"vaddpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (from_register_2, from_register_1, to_register_1)
+    res += ident + '"vsubpd %%%%%s, %%%%%s, %%%%%s\\n"\n' % (from_register_2, from_register_1, to_register_2)
     return res
 
 def float_plain_step(log_n, it, ident=''):
@@ -116,125 +116,69 @@ def double_plain_step(log_n, it, ident=''):
     res += ident + "}\n"
     return res
 
-def float_avx_composite_step(log_n, from_it, to_it, ident=''):
-    if log_n < 3:
-        raise Exception('need at least 8 elements for AVX')
+def composite_step(log_n, from_it, to_it, log_w, registers, move_instruction, special_steps, main_step, ident=''):
+    if log_n < log_w:
+        raise Exception('need at least %d elements' % (1 << log_w))
+    num_registers = len(registers)
+    if num_registers % 2 == 1:
+        raise Exception('odd number of registers: %d' % num_registers)
     num_nontrivial_levels = 0
-    if to_it > 3:
-        first_nontrivial = max(from_it, 3)
+    if to_it > log_w:
+        first_nontrivial = max(from_it, log_w)
         num_nontrivial_levels = to_it - first_nontrivial
-        if num_nontrivial_levels > 3:
+        if 1 << num_nontrivial_levels > num_registers / 2:
             raise Exception('not enough registers')
     n = 1 << log_n
     input_registers = []
     output_registers = []
-    for l in range(8):
-        input_registers.append('%%ymm' + str(l))
-        output_registers.append('%%ymm' + str(8 + l))
+    for i in range(num_registers):
+        if i < num_registers / 2:
+            input_registers.append(registers[i])
+        else:
+            output_registers.append(registers[i])
+    clobber = ', '.join(['"%%%s"' % x for x in registers])
     if num_nontrivial_levels == 0:
-        res  = ident + 'for (int j = 0; j < %d; j += 8) {\n' % n
+        res  = ident + 'for (int j = 0; j < %d; j += %d) {\n' % (n, 1 << log_w)
         res += ident + '  __asm__ volatile (\n'
-        res += ident + '    "vmovups (%s), %s\\n"\n' % ('%0', input_registers[0])
+        res += ident + '    "%s (%%0), %%%%%s\\n"\n' % (move_instruction, input_registers[0])
         for it in range(from_it, to_it):
-            if it == 0:
-                res += float_avx_0(input_registers[0], input_registers[0], output_registers[:4], ident + '    ')
-            if it == 1:
-                res += float_avx_1(input_registers[0], input_registers[0], output_registers[:5], ident + '    ')
-            if it == 2:
-                res += float_avx_2(input_registers[0], input_registers[0], output_registers[:4], ident + '    ')
-        res += ident + '    "vmovups %s, (%s)\\n"\n' % (input_registers[0], '%0')
-        res += ident + '    :: "r"(buf + j) : %s, "memory"\n' % (', '.join(['"%ymm' + str(k) + '"' for k in range(16)]))
+            res += special_steps[it](input_registers[0], output_registers, ident + '    ')
+        res += ident + '    "%s %%%%%s, (%%0)\\n"\n' % (move_instruction, input_registers[0])
+        res += ident + '    :: "r"(buf + j) : %s, "memory"\n' % clobber
         res += ident + '  );\n'
         res += ident + '}\n'
         return res
     res  = ident + 'for (int j = 0; j < %d; j += %d) {\n' % (n, 1 << to_it)
-    res += ident + '  for (int k = 0; k < %d; k += 8) {\n' % (1 << (to_it - num_nontrivial_levels))
+    res += ident + '  for (int k = 0; k < %d; k += %d) {\n' % (1 << (to_it - num_nontrivial_levels), 1 << log_w)
     subcube = []
     for l in range(1 << num_nontrivial_levels):
         subcube.append('j + k + ' + str(l * (1 << (to_it - num_nontrivial_levels))))
     res += ident + '    __asm__ volatile (\n'
     for l in range(1 << num_nontrivial_levels):
-        res += ident + '      "vmovups (%s), %s\\n"\n' % ('%' + str(l), input_registers[l])
-    for it in range(from_it, 3):
+        res += ident + '      "%s (%%%d), %%%%%s\\n"\n' % (move_instruction, l, input_registers[l])
+    for it in range(from_it, log_w):
         for ii in range(1 << num_nontrivial_levels):
-            if it == 0:
-                res += float_avx_0(input_registers[ii], input_registers[ii], output_registers[:4], ident + '      ')
-            if it == 1:
-                res += float_avx_1(input_registers[ii], input_registers[ii], output_registers[:5], ident + '      ')
-            if it == 2:
-                res += float_avx_2(input_registers[ii], input_registers[ii], output_registers[:4], ident + '      ')
+            res += special_steps[it](input_registers[ii], output_registers, ident + '      ')
     for it in range(num_nontrivial_levels):
         for ii in range(0, 1 << num_nontrivial_levels, 1 << (it + 1)):
             for jj in range(1 << it):
-                res += float_avx_3_etc(input_registers[ii + jj], input_registers[ii + jj + (1 << it)], output_registers[ii + jj], output_registers[ii + jj + (1 << it)], ident + '      ')
+                res += main_step(input_registers[ii + jj], input_registers[ii + jj + (1 << it)], output_registers[ii + jj], output_registers[ii + jj + (1 << it)], ident + '      ')
         tmp = input_registers
         input_registers = output_registers
         output_registers = tmp
     for l in range(1 << num_nontrivial_levels):
-        res += ident + '      "vmovups %s, (%s)\\n"\n' % (input_registers[l], '%' + str(l))
-    res += ident + '      :: %s : %s, "memory"\n' % (', '.join(['"r"(buf + %s)' % x for x in subcube]), ', '.join(['"%ymm' + str(k) + '"' for k in range(16)]))
+        res += ident + '      "%s %%%%%s, (%%%d)\\n"\n' % (move_instruction, input_registers[l], l)
+    res += ident + '      :: %s : %s, "memory"\n' % (', '.join(['"r"(buf + %s)' % x for x in subcube]), clobber)
     res += ident + '    );\n'
     res += ident + '  }\n'
     res += ident + '}\n'
     return res
 
+def float_avx_composite_step(log_n, from_it, to_it, ident=''):
+    return composite_step(log_n, from_it, to_it, 3, ['ymm%d' % x for x in range(16)], 'vmovups', [float_avx_0, float_avx_1, float_avx_2], float_avx_3_etc, ident)
+
 def double_avx_composite_step(log_n, from_it, to_it, ident=''):
-    if log_n < 2:
-        raise Exception('need at least 8 elements for AVX')
-    num_nontrivial_levels = 0
-    if to_it > 2:
-        first_nontrivial = max(from_it, 2)
-        num_nontrivial_levels = to_it - first_nontrivial
-        if num_nontrivial_levels > 3:
-            raise Exception('not enough registers')
-    n = 1 << log_n
-    input_registers = []
-    output_registers = []
-    for l in range(8):
-        input_registers.append('%%ymm' + str(l))
-        output_registers.append('%%ymm' + str(8 + l))
-    if num_nontrivial_levels == 0:
-        res  = ident + 'for (int j = 0; j < %d; j += 4) {\n' % n
-        res += ident + '  __asm__ volatile (\n'
-        res += ident + '    "vmovupd (%s), %s\\n"\n' % ('%0', input_registers[0])
-        for it in range(from_it, to_it):
-            if it == 0:
-                res += double_avx_0(input_registers[0], input_registers[0], output_registers[:4], ident + '    ')
-            if it == 1:
-                res += double_avx_1(input_registers[0], input_registers[0], output_registers[:4], ident + '    ')
-        res += ident + '    "vmovupd %s, (%s)\\n"\n' % (input_registers[0], '%0')
-        res += ident + '    :: "r"(buf + j) : %s, "memory"\n' % (', '.join(['"%ymm' + str(k) + '"' for k in range(16)]))
-        res += ident + '  );\n'
-        res += ident + '}\n'
-        return res
-    res  = ident + 'for (int j = 0; j < %d; j += %d) {\n' % (n, 1 << to_it)
-    res += ident + '  for (int k = 0; k < %d; k += 4) {\n' % (1 << (to_it - num_nontrivial_levels))
-    subcube = []
-    for l in range(1 << num_nontrivial_levels):
-        subcube.append('j + k + ' + str(l * (1 << (to_it - num_nontrivial_levels))))
-    res += ident + '    __asm__ volatile (\n'
-    for l in range(1 << num_nontrivial_levels):
-        res += ident + '      "vmovupd (%s), %s\\n"\n' % ('%' + str(l), input_registers[l])
-    for it in range(from_it, 2):
-        for ii in range(1 << num_nontrivial_levels):
-            if it == 0:
-                res += double_avx_0(input_registers[ii], input_registers[ii], output_registers[:4], ident + '      ')
-            if it == 1:
-                res += double_avx_1(input_registers[ii], input_registers[ii], output_registers[:4], ident + '      ')
-    for it in range(num_nontrivial_levels):
-        for ii in range(0, 1 << num_nontrivial_levels, 1 << (it + 1)):
-            for jj in range(1 << it):
-                res += double_avx_2_etc(input_registers[ii + jj], input_registers[ii + jj + (1 << it)], output_registers[ii + jj], output_registers[ii + jj + (1 << it)], ident + '      ')
-        tmp = input_registers
-        input_registers = output_registers
-        output_registers = tmp
-    for l in range(1 << num_nontrivial_levels):
-        res += ident + '      "vmovupd %s, (%s)\\n"\n' % (input_registers[l], '%' + str(l))
-    res += ident + '      :: %s : %s, "memory"\n' % (', '.join(['"r"(buf + %s)' % x for x in subcube]), ', '.join(['"%ymm' + str(k) + '"' for k in range(16)]))
-    res += ident + '    );\n'
-    res += ident + '  }\n'
-    res += ident + '}\n'
-    return res
+    return composite_step(log_n, from_it, to_it, 2, ['ymm%d' % x for x in range(16)], 'vmovupd', [double_avx_0, double_avx_1], double_avx_2_etc, ident)
 
 def float_avx_greedy_merged(log_n):
     res  = "inline void helper_%d(float *buf);\n" % log_n
